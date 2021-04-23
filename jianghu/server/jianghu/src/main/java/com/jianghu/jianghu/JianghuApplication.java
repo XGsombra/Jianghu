efe917c923e0415cec2c56aa8b2fdc3c1ac4a019
XGsombra <xuduo.gu@mail.utoronto.ca>
2021-04-22 20:35:43 -0400

    Added starter code

---
+++ jianghu/server/jianghu/src/main/java/com/jianghu/jianghu/JianghuApplication.java
@@ -0,0 +1,13 @@
+package com.jianghu.jianghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JianghuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JianghuApplication.class, args);
+	}
+
+}
---
+++ jianghu/server/jianghu/src/test/java/com/jianghu/jianghu/JianghuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jianghu.jianghu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JianghuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
