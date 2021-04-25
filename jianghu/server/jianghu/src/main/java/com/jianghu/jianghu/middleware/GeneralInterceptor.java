8f529be2c61fea8b4e513c3f3f80fc3b84496f05
XGsombra <xuduo.gu@mail.utoronto.ca>
2021-04-25 13:44:51 -0400

    Added http response to the log

--- jianghu/server/jianghu/src/main/java/com/jianghu/jianghu/middleware/GeneralInterceptor.java
+++ jianghu/server/jianghu/src/main/java/com/jianghu/jianghu/middleware/GeneralInterceptor.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -55,9 +56,57 @@ public class GeneralInterceptor implements HandlerInterceptor {
             logFile.getParentFile().mkdirs();
             logFile.createNewFile();
         }
+        FileWriter fileWriter = new FileWriter(logFile, true);
+        fileWriter.write(logItem.toString());
+        fileWriter.close();
+        return true;
+    }
+
+//    @Override
+//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//        // log the http request
+//        StringBuffer logItem = new StringBuffer();
+//        // add the request time to log item
+//        logItem.append("response time: " + new Date() + "\n");
+//        logItem.append("HTTP status:   " + response.getStatus() + "\n");
+//        logItem.append("" + response.)
+//        logItem.append("---------------------------------------------------\n");
+//
+//        File logFile = new File(httpLogPath);
+//        Boolean existsLogFile = logFile.exists();
+//
+//        if (!existsLogFile){
+//            logFile.getParentFile().mkdirs();
+//            logFile.createNewFile();
+//        }
+//        FileWriter fr = new FileWriter(logFile, true);
+//        fr.write(logItem.toString());
+//        fr.close();
+//    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // log the http request
+        StringBuffer logItem = new StringBuffer();
+        // add the request time to log item
+        logItem.append("response time: " + new Date() + "\n");
+        // add the session id to log item
+        logItem.append("session ID:   " + request.getSession().getId() + "\n");
+        // add the user id to log item
+        logItem.append("user ID:      " + request.getSession().getAttribute("userId") + "\n");
+        // add the http response status to log item
+        logItem.append("HTTP status:  " + response.getStatus() + "\n");
+        logItem.append("---------------------------------------------------\n");
+
+        File logFile = new File(httpLogPath);
+        Boolean existsLogFile = logFile.exists();
+
+        if (!existsLogFile){
+            logFile.getParentFile().mkdirs();
+            logFile.createNewFile();
+        }
         FileWriter fr = new FileWriter(logFile, true);
         fr.write(logItem.toString());
         fr.close();
-        return true;
     }
 }
